8b81a7249ec26870945d75f9c000524762dcdbee
Häckklipparen Janne <skedis123@gmail.com>
2019-10-03 23:04:34 +0200

    spaghet code azweusftaegjrgaftrsftghstrgyhtrf

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes');
+const errorhandler = require('errorhandler');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(errorhandler());
 
 // Connecting all endpoints
 app.use(routes);
